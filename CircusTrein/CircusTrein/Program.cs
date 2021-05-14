de006f668c6ff34909dcf50f0fb22e022c86f667
RIk <Rikdenbreejen@hotmail.nl>
2021-05-14 11:18:35 +0200

    [Change] Animal names are now english

--- CircusTrein/CircusTrein/Program.cs
+++ CircusTrein/CircusTrein/Program.cs
@@ -9,18 +9,18 @@ namespace CircusTrein.Front
         static void Main(string[] args)
         {
             var Animals = new List<Animal>();
-            Animals.Add(new Animal("Tijger", Foodtype.Carnivore, AnimalSize.Big));
-            Animals.Add(new Animal("Tijger", Foodtype.Carnivore, AnimalSize.Big));
+            Animals.Add(new Animal("Tiger", Foodtype.Carnivore, AnimalSize.Big));
+            Animals.Add(new Animal("Tiger", Foodtype.Carnivore, AnimalSize.Big));
             Animals.Add(new Animal("Maki", Foodtype.Herbivore, AnimalSize.Small));
             Animals.Add(new Animal("Maki", Foodtype.Herbivore, AnimalSize.Small));
             Animals.Add(new Animal("Maki", Foodtype.Herbivore, AnimalSize.Small));
-            Animals.Add(new Animal("Stokstaartje", Foodtype.Herbivore, AnimalSize.Small));
-            Animals.Add(new Animal("Leeuw", Foodtype.Carnivore, AnimalSize.Big));
+            Animals.Add(new Animal("Meerkat", Foodtype.Herbivore, AnimalSize.Small));
+            Animals.Add(new Animal("Lion", Foodtype.Carnivore, AnimalSize.Big));
             Animals.Add(new Animal("Hyena", Foodtype.Carnivore, AnimalSize.Medium));
             Animals.Add(new Animal("Hyena", Foodtype.Carnivore, AnimalSize.Medium));
-            Animals.Add(new Animal("Olifant", Foodtype.Herbivore, AnimalSize.Big));
-            Animals.Add(new Animal("Olifant", Foodtype.Herbivore, AnimalSize.Big));
-            Animals.Add(new Animal("Olifant", Foodtype.Herbivore, AnimalSize.Big));
+            Animals.Add(new Animal("Elephant", Foodtype.Herbivore, AnimalSize.Big));
+            Animals.Add(new Animal("Elephant", Foodtype.Herbivore, AnimalSize.Big));
+            Animals.Add(new Animal("Elephant", Foodtype.Herbivore, AnimalSize.Big));
 
             var CircusTrain = new Train();
             CircusTrain.LoadAnimals(Animals);
