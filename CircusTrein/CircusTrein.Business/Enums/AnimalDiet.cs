76f623e61addb9f4fb8667049167c944e437b961
RikThePixel <Rikdenbreejen@hotmail.nl>
2021-05-26 11:39:39 +0200

    [Change] Ordered Enums

---
+++ CircusTrein/CircusTrein.Business/Enums/AnimalDiet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircusTrein.Business
+{
+    public enum AnimalDiet
+    {
+        Carnivore,
+        Herbivore
+    }
+}
---
+++ CircusTrein/CircusTrein.Business/Enums/AnimalSize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircusTrein.Business
+{
+    public enum AnimalSize
+    {
+        Small = 1,
+        Medium = 3,
+        Big = 5
+    }
+}
