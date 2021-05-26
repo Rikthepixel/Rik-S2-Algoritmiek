5af16846d1e3effbb33c25e9eea493bf095796e1
RikThePixel <Rikdenbreejen@hotmail.nl>
2021-05-26 12:07:09 +0200

    [Addition] Cart rules unit test

--- CircusTrein/CircusTrein.Unit/CartTests.cs
+++ CircusTrein/CircusTrein.Unit/CartTests.cs
@@ -24,5 +24,29 @@ namespace CircusTrein.Unit
 
             Assert.IsFalse(Cart.CheckAndAddAnimal(Herbivore));
         }
+
+        [Test]
+        public void Larger_Carnivore_And_Smaller_Herbivore_Cant_Fit_Together()
+        {
+            var Carnivore = new Animal("Tiger", AnimalDiet.Carnivore, AnimalSize.Big);
+            var Herbivore = new Animal("Deer", AnimalDiet.Herbivore, AnimalSize.Medium);
+
+            var Cart = new Cart(10);
+            Cart.CheckAndAddAnimal(Carnivore);
+
+            Assert.IsFalse(Cart.CheckAndAddAnimal(Herbivore));
+        }
+
+        [Test]
+        public void Smaller_Carnivore_And_Larger_Herbivore_Can_Fit_Together()
+        {
+            var Carnivore = new Animal("Hyena", AnimalDiet.Carnivore, AnimalSize.Medium);
+            var Herbivore = new Animal("Elephant", AnimalDiet.Herbivore, AnimalSize.Big);
+
+            var Cart = new Cart(10);
+            Cart.CheckAndAddAnimal(Carnivore);
+
+            Assert.IsTrue(Cart.CheckAndAddAnimal(Herbivore));
+        }
     }
 }
