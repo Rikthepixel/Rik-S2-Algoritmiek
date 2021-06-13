92f8284b69af2d57ea81a3d27461652a91e470ae
RIk <Rikdenbreejen@hotmail.nl>
2021-06-13 20:00:43 +0200

    [Addition] Animal unittests

---
+++ CircusTrein/CircusTrein.Unit/AnimalTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using CircusTrein.Business;
+using CircusTrein.Business.Factory;
+using System.Collections.Generic;
+
+namespace CircusTrein.Unit
+{
+    class AnimalTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Same_Size_Carnivore_And_Herbivore_Cant_Be_Paired()
+        {
+
+            var Carnivore = AnimalFactory.CreateBigCarnivore();
+            var Herbivore = AnimalFactory.CreateBigHerbivore();
+
+            Assert.IsFalse(Carnivore.CanBePaired(Herbivore));
+        }
+    }
+}
